a79c6f16e21d215011f28ff36c28e5d0254a5234
vitordeatorreao <vitordeatorreao@gmail.com>
2015-09-16 21:02:37 -0300

    Implementação da seleção por ganho de informação e por correlação

--- src/main/java/br/com/phendia/vitor/rp/lista2/IonosphereFilter.java
+++ src/main/java/br/com/phendia/vitor/rp/lista2/IonosphereFilter.java
@@ -1,6 +1,9 @@
 package br.com.phendia.vitor.rp.lista2;
 
+import weka.attributeSelection.BestFirst;
+import weka.attributeSelection.CfsSubsetEval;
 import weka.attributeSelection.ChiSquaredAttributeEval;
+import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -27,10 +30,31 @@ public class IonosphereFilter
 		return Filter.useFilter(data, filter);
 	}
 	
-	public static void printAttributes(Instances data) {
-		for (int i = 0; i < data.numAttributes(); i++) {
-			System.out.println(data.attribute(i).name());
+	public Instances infoGainFilter() throws Exception {
+		AttributeSelection filter = new AttributeSelection();
+		Ranker search = new Ranker();
+		filter.setSearch(search);
+		filter.setInputFormat(data);
+		InfoGainAttributeEval evaluator = new InfoGainAttributeEval();
+		filter.setEvaluator(evaluator);
+		return Filter.useFilter(data, filter);
+	}
+	
+	public Instances correlationFilter() throws Exception {
+		AttributeSelection filter = new AttributeSelection();
+		BestFirst search = new BestFirst();
+		filter.setSearch(search);
+		filter.setInputFormat(data);
+		CfsSubsetEval evaluator = new CfsSubsetEval();
+		filter.setEvaluator(evaluator);
+		return Filter.useFilter(data, filter);
+	}
+	
+	public static void printAttributes(Instances data, int rank) {
+		for (int i = 0; i < rank; i++) {
+			System.out.print(data.attribute(i).name());
 		}
+		System.out.print("\n");
 	}
 
 	public Instances getData() {
@@ -41,9 +65,17 @@ public class IonosphereFilter
     {
         try {
 			IonosphereFilter lista2 = new IonosphereFilter();
-			IonosphereFilter.printAttributes(lista2.getData());
+			System.out.println("Todos os atributos:");
+			IonosphereFilter.printAttributes(lista2.getData(), lista2.getData().numAttributes());
 			Instances data = lista2.chiSquareFilter();
-			IonosphereFilter.printAttributes(data);
+			System.out.println("Chi Quadrado:");
+			IonosphereFilter.printAttributes(data, 5);
+			Instances data2 = lista2.correlationFilter();
+			System.out.println("Baseado em correlação:");
+			IonosphereFilter.printAttributes(data2, 5);
+			Instances data3 = lista2.infoGainFilter();
+			System.out.println("Baseado em Ganho de Informação:");
+			IonosphereFilter.printAttributes(data3, 5);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
